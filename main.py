05e4d1e24af26bebd757413120fa88b15e8f651d
ezekaj <elvizekaj02@gmail.com>
2026-02-10 01:22:34 +0100

    Disable uvicorn reload for production safety

--- main.py
+++ main.py
@@ -249,4 +249,4 @@ async def websocket_prices(websocket: WebSocket):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=False)
